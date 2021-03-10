eb897448fb7439fb05a0ecc15ee682e8dcbc92a6
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2021-03-11 08:11:16 +0900

    file handling

---
+++ graph_algorithm/kargermincut.py
@@ -0,0 +1,61 @@
+from linkedlist import Node,LinkedList
+import re,sys,shutil
+
+
+import random
+
+def random_line(afile):
+    line = next(afile)
+    flag = re.match("\n+",line)
+    print(f"flag is {flag}")
+    while flag is not None:
+        print(f"line {line}")
+        line = re.sub("\n+","",line)
+        flag = re.match("\n+",line)
+        print(f"flag is {flag}")
+        
+    for num, aline in enumerate(afile, 2):
+        print(f"num {num} aline {aline}")
+        if random.randrange(num):
+            continue
+        if not aline=="\n":
+            line = aline
+    return line
+
+if __name__=="__main__":
+
+    with open("test_case1.txt", "r") as f:
+        randline = random_line(f)
+        print(f"randline {randline}")
+    with open("test_case1.txt", "r") as f:
+        lines = f.readlines()
+    with open("test_case1.txt", "w") as f:
+        for line in lines:
+            line = re.sub(r"\n+","\n",line).strip("\n")
+            randline=randline.strip("\n")
+            if line != randline:
+                print(f"line {line}")
+                line=line+"\n"
+                f.write(line)
+
+    sys.exit()
+
+    f = open("test_case1.txt",mode="r")
+
+    l = random_line(f)
+    print(f"random line {l.split()}")
+
+    with open("test_case1.txt","r+") as fs:
+        for c in fs:
+            if not c.strip("\n")==l:
+                print(c)
+                fs.write(c)
+    
+    with open("test_case1.txt","r") as lines:
+        for line in lines:
+            print(line)
+    
+    #ll1=LinkedList(l.split())
+    #print(ll1)
+
+
---
+++ graph_algorithm/linkedlist.py
@@ -0,0 +1,153 @@
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return self.data
+
+class LinkedList:
+    def __init__(self,nodes=None):
+        self.head = None
+
+        if nodes is not None:
+            node = Node(nodes.pop(0))
+            self.head = node
+            
+            for elem in nodes:
+                node.next = Node(data=elem)
+                node = node.next
+    
+    def __repr__(self):
+        nodelist=[]
+        node = self.head
+        while node is not None:
+            nodelist.append(node.data)
+            node = node.next
+        nodelist.append("None")
+        return "->".join(nodelist)
+
+    def __iter__(self):
+        node = self.head
+
+        while node is not None:
+            yield node
+            node=node.next 
+
+    def add_first(self,node):
+        node.next = self.head
+        self.head = node
+
+    
+    def add_last(self,node):
+        if self.head is None:
+            self.head = node
+            return
+        for current_node in self:
+            pass
+        current_node.next = node
+
+    def add_after(self,targetnode, newnode):
+        if self.head is None:
+            raise Exception("Empty List")
+        
+        for current_node in self:
+            if current_node.data==targetnode:
+                newnode.next = current_node.next
+                current_node.next = newnode
+                return 
+        
+        raise Exception(f"{targetnode} is not in the list")
+
+    #def add_before(self,targetnode,newnode):
+    #    if self.head is None:
+    #        raise Exception("Empty list")
+    #    
+    #    prevnode=self.head
+    #    for current_node in self:
+    #        print(f"current_node {current_node.data}")
+    #        if current_node.data==targetnode:
+    #            prevnode.next=newnode
+    #            newnode.next=current_node
+    #        prevnode=current_node
+
+    #    raise Exception(f"{targetnode} not in the list")
+
+    def add_before(self, target_node_data, new_node):
+        if self.head is None:
+            raise Exception("List is empty")
+
+        if self.head.data == target_node_data:
+            return self.add_first(new_node)
+
+        prev_node = self.head
+        for node in self:
+            if node.data == target_node_data:
+                prev_node.next = new_node
+                new_node.next = node
+                return
+            prev_node = node
+        raise Exception(f"{target_node_data} not in the list")
+
+    
+    def remove_node(self,targetnode_data):
+        if self.head is None:
+            raise Exception("Empty list")
+        if self.head.data==targetnode_data:
+            self.head = self.head.next
+            return
+        
+        prevnode=self.head
+        for current_node in self:
+            if current_node.data==targetnode_data:
+                prevnode.next = current_node.next
+                return
+
+            prevnode=current_node
+
+
+
+if __name__=="__main__":
+    ll1 = LinkedList(nodes=["a","b","c","d"])
+    ll1.add_first(Node("first"))
+    ll1.add_last(Node("last"))
+    ll1.add_after("b",Node("after"))
+    ll1.add_before("b",Node("before"))
+    print(ll1)
+
+    for l in ll1:
+        print(l)
+    
+    f = open("test_case1.txt","r")
+
+    for line in f:
+        print(line)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
