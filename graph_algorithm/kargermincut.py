d595ef9de4ba99171f83a72626be9a32285ec4b7
ahmedmustahid <ahmed@epx.phys.tohoku.ac.jp>
2021-03-13 19:37:06 +0900

    choose line randomly

--- graph_algorithm/kargermincut.py
+++ graph_algorithm/kargermincut.py
@@ -15,28 +15,48 @@ def random_line(afile):
         print(f"flag is {flag}")
         
     for num, aline in enumerate(afile, 2):
-        print(f"num {num} aline {aline}")
         if random.randrange(num):
+            print(f"inside randrange {num}")
             continue
-        if not aline=="\n":
+        if not re.match(r"\n+",aline):
             line = aline
+            print(f"num {num} aline {aline}")
     return line
+
+#def getline(afile,randline):
+#    for line in afile:
+#        yield line
+
 
 if __name__=="__main__":
 
     with open("test_case1.txt", "r") as f:
         randline = random_line(f)
         print(f"randline {randline}")
-    with open("test_case1.txt", "r") as f:
-        lines = f.readlines()
-    with open("test_case1.txt", "w") as f:
+    #with open("test_case1.txt", "r") as f:
+    #    lines = f.readlines()
+    #    #line_count=sum()
+    f1 = open("test_case1.txt","r+")
+
+    gline=(line for line in f1 if not line==randline)
+     
+    with open("test_case2.txt","w") as f2:
+        for line in gline:
+            print(f"gline {line}")
+            f2.write(line)
+
+    f1.close()
+
+    sys.exit()
+    with open("test_case1.txt", "r+") as lines:
         for line in lines:
             line = re.sub(r"\n+","\n",line).strip("\n")
             randline=randline.strip("\n")
-            if line != randline:
-                print(f"line {line}")
-                line=line+"\n"
-                f.write(line)
+            if line == randline:
+                line.strip(line)
+                print(f"line {line} is stripped")
+                #line=line+"\n"
+                lines.write(line)
 
     sys.exit()
 
---
+++ graph_algorithm/mmap_replace.py
@@ -0,0 +1,73 @@
+import random
+import mmap
+import re,os,shutil
+
+
+def find_and_delete(filename,randline):
+    
+
+    with open(filename, "r", encoding="utf-8") as orig_file_obj:
+        with open("tmp.txt", "w", encoding="utf-8") as new_file_obj:
+            orig_text = orig_file_obj.read()
+            randline=randline+"\n"
+            new_text = orig_text.replace(randline, "")
+            #new_text = orig_text.replace(randline, "")
+            new_file_obj.write(new_text)
+
+    shutil.copyfile("tmp.txt", filename)
+    os.remove("tmp.txt")
+
+
+
+def mmap_io(filename,randline):
+    with open(filename, mode="r+", encoding="utf8") as file_obj:
+        with mmap.mmap(file_obj.fileno(), length=0, access=mmap.ACCESS_WRITE) as mmap_obj:
+            text = mmap_obj.read()
+            randline=randline.encode('utf_8')
+            new_text = text.replace(randline, b"")
+            
+            #print(f"new txt {new_text}")
+            mmap_obj[:]=new_text
+            mmap_obj.flush()
+            #while True:
+            #    line = mmap_obj.readline()
+            #    print(line)
+            #    if not line:
+            #        break
+
+
+
+
+def random_line(filename):
+    with open(filename,"r") as afile:
+        line = next(afile)
+        for num, aline in enumerate(afile, 2):
+            if random.randrange(num):
+                #print(f"inside randrange {num}")
+                continue
+            if not re.match(r"\n+",aline):
+                line = re.sub(r"\n+","",string=aline)
+                #print(f"num {num} aline {aline}")
+    return line
+
+
+if __name__=="__main__":
+    
+    filename="test_case1.txt" 
+    #filename="KargerMinCut_challenge_case.txt" 
+    line_count =sum(line.count('\n') for line in open(filename,"r"))
+
+    while line_count>2:
+        randline = random_line(filename)
+        print(f"randline {randline}")
+        find_and_delete(filename,randline)
+        line_count =sum(line.count('\n') for line in open(filename,"r"))
+        print(f"line_count {line_count}")
+    else:
+        print(f"line_count {line_count} is minimum")
+
+
+
+
+
+
