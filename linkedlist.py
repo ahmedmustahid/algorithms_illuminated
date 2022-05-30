f50da29b066535ee863bd65e607ee6691148dff7
ahmedmustahid <einsteinium25@gmail.com>
2022-05-30 19:59:45 +0900

    naive implementation of toposort with iterative dfs

--- linkedlist.py
+++ linkedlist.py
@@ -1,26 +1,26 @@
-
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
     def __repr__(self):
         return self.data
 
+
 class LinkedList:
-    def __init__(self,nodes=None):
+    def __init__(self, nodes=None):
         self.head = None
 
         if nodes is not None:
             node = Node(nodes.pop(0))
             self.head = node
-            
+
             for elem in nodes:
                 node.next = Node(data=elem)
                 node = node.next
-    
+
     def __repr__(self):
-        nodelist=[]
+        nodelist = []
         node = self.head
         while node is not None:
             nodelist.append(node.data)
@@ -33,14 +33,13 @@ class LinkedList:
 
         while node is not None:
             yield node
-            node=node.next 
+            node = node.next
 
-    def add_first(self,node):
+    def add_first(self, node):
         node.next = self.head
         self.head = node
 
-    
-    def add_last(self,node):
+    def add_last(self, node):
         if self.head is None:
             self.head = node
             return
@@ -48,22 +47,22 @@ class LinkedList:
             pass
         current_node.next = node
 
-    def add_after(self,targetnode, newnode):
+    def add_after(self, targetnode, newnode):
         if self.head is None:
             raise Exception("Empty List")
-        
+
         for current_node in self:
-            if current_node.data==targetnode:
+            if current_node.data == targetnode:
                 newnode.next = current_node.next
                 current_node.next = newnode
-                return 
-        
+                return
+
         raise Exception(f"{targetnode} is not in the list")
 
-    #def add_before(self,targetnode,newnode):
+    # def add_before(self,targetnode,newnode):
     #    if self.head is None:
     #        raise Exception("Empty list")
-    #    
+    #
     #    prevnode=self.head
     #    for current_node in self:
     #        print(f"current_node {current_node.data}")
@@ -90,60 +89,29 @@ class LinkedList:
             prev_node = node
         raise Exception(f"{target_node_data} not in the list")
 
-    
-    def remove_node(self,targetnode_data):
+    def remove_node(self, targetnode_data):
         if self.head is None:
             raise Exception("Empty list")
-        if self.head.data==targetnode_data:
+        if self.head.data == targetnode_data:
             self.head = self.head.next
             return
-        
-        prevnode=self.head
+
+        prevnode = self.head
         for current_node in self:
-            if current_node.data==targetnode_data:
+            if current_node.data == targetnode_data:
                 prevnode.next = current_node.next
                 return
 
-            prevnode=current_node
+            prevnode = current_node
 
 
-
-if __name__=="__main__":
-    ll1 = LinkedList(nodes=["a","b","c","d"])
+if __name__ == "__main__":
+    ll1 = LinkedList(nodes=["a", "b", "c", "d"])
     ll1.add_first(Node("first"))
     ll1.add_last(Node("last"))
-    ll1.add_after("b",Node("after"))
-    ll1.add_before("b",Node("before"))
+    ll1.add_after("b", Node("after"))
+    ll1.add_before("b", Node("before"))
     print(ll1)
 
     for l in ll1:
         print(l)
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
