1b4ccca552ca7e922f9955293714a8d107243def
ahmedmustahid <einsteinium25@gmail.com>
2022-06-03 20:00:15 +0900

    iterative dfs

--- graph_algorithm/kosaraju.py
+++ graph_algorithm/kosaraju.py
@@ -1,101 +1,85 @@
-import time
+from pathlib import Path
 import pprint
-#from memory_profiler import profile
-#import tracemalloc
+import collections
+import sys
+import tracemalloc 
+
+
+pp = pprint.PrettyPrinter(width=41, compact=True)
 
 #tracemalloc.start()
-
 def graphFromFile(fileName):
+    graph = collections.defaultdict(lambda: {'children': [], 'explored': False, 'label': 0})
+    for line in open(fileName, "r"):
+        k, v = map(int, line.split())
+        if k != v:
+            graph[k]['children'].append(v)
+    return graph
+#fileName = Path.home()/"work/algorithms_illuminated/graph_algorithm/scc/test_cases/testCase1.txt"
+fileName = Path.cwd()/"testCase2.txt"
+graph = graphFromFile(fileName)
+currentLabel = len(graph)
+dfsNum = 0
+labelDict = dict.fromkeys(range(currentLabel, 0, -1))
 
-    startTime = time.time()
-    temp = "1" 
-    d = {temp:[]}
-    heads = []
-    with open(fileName,"r") as f:
-        for line in f:
-            tail, head = line.split()
+def reversedGraph(graph):
+    reversedGraph = collections.defaultdict(lambda: {'children': [], 'explored': False})
+    for vertex in graph:
+        for child in graph[vertex]['children']:
+            reversedGraph[child]['children'].append(vertex)
+    return reversedGraph
 
-            if tail == head :
-                continue  
-            if temp == tail:
-                d[temp].append(head)
-                heads.append(head)
-            else:
-                temp = tail
-                #d = {temp:[head]}
-                d[temp] = [head]
-                heads.append(head)
-    
-    for head in heads:
-        if head not in d:
-            d[head] = []
-            #print(head)
-            #break
-    #pprint.pprint(d)
-    #pprint.pprint(d[temp])
-    print("--- %s seconds ---" % (time.time() - startTime))
-    return d
-
-#class TopoSort:
-#    def __init__(graphDict, exploredNodeDict, topoSortNumDict):
-#        self.graphDict = graphDict
-#        self.exploredNodeDict = exploredNodeDict
-#        self.topoSortNumDict = topoSortNumDict
-#        self.depth = len(graphDict)
-#
-#    def dfstopo(self):
-#fileName ="scc/sccChallenge.txt"
-fileName ="scc/test_cases/testCase1.txt"
-graphDict = graphFromFile(fileName)
-exploredNodeDict = dict.fromkeys(graphDict, False)
-toposortNum = dict.fromkeys(graphDict)
-#depth = len(graphDict)
-
-def dfstopo(node):
-
-    global depth
-    #global graphDict
-    # if node not in graphDict:
-    #     return
-    if exploredNodeDict[node] is False:
-        exploredNodeDict[node] = True
-    else:
-        return
-    
-    for adjacentNode in graphDict[node]:
-        dfstopo(adjacentNode)
-    
-    toposortNum[node] = depth
-    depth = depth - 1
-    #print(depth)
-    return 
-
-def topoSort(graphDict):
-    global depth
-    depth = len(graphDict)
-    for node in graphDict:
-        if exploredNodeDict[node] is False:
-            dfstopo(node)
-
-#if __name__=="__main__":
+def dfs(graph, vertex):
+    global currentLabel
+    global dfsNum
+    #print(f"{dfsNum} entering at {vertex}")
+    print(f"entering at {vertex}")
+    dfsNum = dfsNum + 1
+    graph[vertex]['explored'] = True
+    for child in graph[vertex]['children']:
+        print(f"child {child} of vertex {vertex}")
+        if not graph[child]['explored']:
+            dfs(graph, child)
+        else:
+            print(f"child {child} already explored")
+    graph[vertex]['label'] = currentLabel
+    labelDict[currentLabel] = vertex
+    currentLabel = currentLabel -1
+    print(f"exiting from {vertex}")
 
 
-#pprint.pprint(exploredNodeDict)
-#pprint.pprint(toposortNum)
-#print(f"total number of nodes {depth}")
-#dfstopo(graphDict=graphDict, node = '1')
+def iterativeDFS(graph):
+    stack = [6]
+    exploreOrder = []
+    while stack:
+        poppedNode = stack.pop()
+        if not graph[poppedNode]['explored']:
+            graph[poppedNode]['explored'] = True
+            stack += graph[poppedNode]['children']
+            exploreOrder.append(poppedNode)
+            #for child in graph[poppedNode]['children']:
+    print(exploreOrder)
 
-topoSort(graphDict=graphDict)
 
-#mygraph = graphFromFile(fileName)
-# if '416780' in graphDict.keys():
-#     print("yes")
-# else:
-#     print("no")
 
-# print(tracemalloc.get_traced_memory())
-# tracemalloc.stop()
-pprint.pprint(graphDict)
-pprint.pprint(exploredNodeDict)
+def toposort():
+    for vertex in graph:
+        if not graph[vertex]['explored']:
+            print(f"in toposort {vertex}")
+            dfs(graph, vertex)
 
-pprint.pprint(toposortNum)
+
+if __name__=="__main__":
+    #print(tracemalloc.get_traced_memory())
+    #dfs(vertex=2)
+    #pp.pprint(reversedGraph(graph))
+    #toposort()
+    #tracemalloc.stop()
+    #pp.pprint(labelDict)
+    pp.pprint(graph)
+    iterativeDFS(graph)
+
+
+
+
+
