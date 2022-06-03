b06134528d30e4514dcd298e23fa162a96559057
ahmedmustahid <einsteinium25@gmail.com>
2022-06-03 17:22:11 +0900

    recursive dfs and reverse graph

--- graph_algorithm/graph.py
+++ graph_algorithm/graph.py
@@ -2,55 +2,61 @@ from pathlib import Path
 import pprint
 import collections
 import sys
+import tracemalloc 
+
 
 pp = pprint.PrettyPrinter(width=41, compact=True)
+
+tracemalloc.start()
+def graphFromFile(fileName):
+    graph = collections.defaultdict(lambda: {'children': [], 'explored': False, 'label': 0})
+    for line in open(fileName, "r"):
+        k, v = map(int, line.split())
+        if k != v:
+            graph[k]['children'].append(v)
+    return graph
 fileName = Path.home()/"work/algorithms_illuminated/graph_algorithm/scc/test_cases/testCase1.txt"
-#fileName = Path.home()/"work/sccTestCase.txt"
-
-#graphDict = {}
-#graph = collections.defaultdict(lambda: {'g': set(), 's': False, 't': None, 'u': None })
-graph = collections.defaultdict(lambda: {'g': [], 's': False, 'label': 0})
-#graph = collections.defaultdict(lambda: {'g': [], 's': False, 't': None, 'u': None })
-for line in open(fileName, "r"):
-    #print(line)
-    k, v = map(int, line.split())
-    #print(f"{k}:{v}")
-    if k != v:
-        graph[k]['g'].append(v)
-    #graph[k]['g'].add(v)
-
-
-pp.pprint(graph)
-#for k in graph:
-#    pp.pprint(graph[k])
-#    #print(k)
-    
-#for key in graph.keys():
-#    print(key)
-#    break
-
+graph = graphFromFile(fileName)
 currentLabel = len(graph)
-print(f"currentLabel {currentLabel}")
+dfsNum = 0
 
-for key in graph.keys():
-    stack = [key]
-    exploredNodes = [] 
-    while stack:
-        poppedNode = stack.pop()
-        #pp.pprint(v)
-        if not graph[poppedNode]['s']:
-            graph[poppedNode]['s'] = True
-            exploredNodes.append(poppedNode)
-    
-            for w in graph[poppedNode]['g']:
-                stack.append(w)
+def reversedGraph(graph):
+    reversedGraph = collections.defaultdict(lambda: {'children': [], 'explored': False, 'label': 0})
+    for vertex in graph:
+        for child in graph[vertex]['children']:
+            reversedGraph[child]['children'].append(vertex)
+    return reversedGraph
 
-    while exploredNodes:
-        exploredNode = exploredNodes.pop()
-        graph[exploredNode]['label'] = currentLabel
-        currentLabel = currentLabel - 1
+def dfs(vertex):
+    global currentLabel
+    global dfsNum
+    #print(f"{dfsNum} entering at {vertex}")
+    print(f"entering at {vertex}")
+    dfsNum = dfsNum + 1
+    graph[vertex]['explored'] = True
+    for child in graph[vertex]['children']:
+        print(f"child {child} of vertex {vertex}")
+        if not graph[child]['explored']:
+            dfs(child)
+        else:
+            print(f"child {child} already explored")
+    graph[vertex]['label'] = currentLabel
+    currentLabel = currentLabel -1
+    print(f"exiting from {vertex}")
+
+def toposort():
+    for vertex in graph:
+        dfs(vertex)
 
 
-for key in graph.keys():
-    print(f"{key}:label {graph[key]['label']}")
-#pp.pprint(graph)
+if __name__=="__main__":
+    print(tracemalloc.get_traced_memory())
+    dfs(vertex=2)
+    pp.pprint(graph)
+    pp.pprint(reversedGraph(graph))
+    tracemalloc.stop()
+
+
+
+
+
