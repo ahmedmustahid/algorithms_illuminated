a83ea27f41f822092dc5ffa91ff6a081f8fe3aff
ahmedmustahid <einsteinium25@gmail.com>
2022-06-23 17:12:30 +0900

    heap implementation dijkstra

---
+++ graph_algorithm/Dijkstra/dijkstraHeap.py
@@ -0,0 +1,101 @@
+import collections
+from pathlib import Path
+import pprint
+import sys
+import networkx as nx
+import matplotlib.pyplot as plt
+import more_itertools
+import heapq
+pp = pprint.PrettyPrinter(width=41, compact=True)
+#fileName = Path.home()/"work/algorithms_illuminated/graph_algorithm/Dijkstra/testCases/dijkstraChallenge.txt"
+fileName = Path.home()/"work/algorithms_illuminated/graph_algorithm/Dijkstra/testCases/testCase1.txt"
+
+def createGraph(fileName):
+    graph = collections.defaultdict(lambda : {'children': {}, 'length': 1000000})
+    with open(fileName) as f:
+        for line in f:
+            test = line.split()
+            #print(test)
+            for child in test[1:]:
+                node, weight = map(int, child.split(','))
+                graph[int(test[0])]['children'][node] = weight
+    #pp.pprint(graph)
+    return graph
+
+def visualizeGraph(fileName):
+    G = nx.DiGraph()
+    graph = createGraph(fileName)
+
+    for head in graph:
+        for tail, weight in graph[head]['children'].items():
+            G.add_edge(head, tail, weight = weight)
+    print(G.size())
+    print(G.number_of_nodes())
+    pos = nx.spring_layout(G, seed=7)
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=700)
+    # edges
+    nx.draw_networkx_edges(G, pos, width=6)
+    # node labels
+    nx.draw_networkx_labels(G, pos, font_size=20, font_family="sans-serif")
+    # edge weight labels
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos, edge_labels)
+
+    ax = plt.gca()
+    ax.margins(0.08)
+    plt.axis("off")
+    plt.tight_layout()
+    #plt.show()
+    imageName = str(fileName.parent) +"/" + fileName.stem + ".png"
+    plt.savefig(imageName)
+    print(f"{imageName} saved")
+
+#visualizeGraph(fileName= fileName)
+
+def dijkstraPathNetworkx(graph, tails):
+    G = nx.DiGraph()
+    #graph = createGraph(fileName)
+
+    for head in graph:
+        for tail, weight in graph[head]['children'].items():
+            G.add_edge(head, tail, weight = weight)
+    dijkstraLens = []
+    for tail in tails:
+        distance, _ = nx.single_source_dijkstra(G ,source=1, target= tail)
+        dijkstraLens.append(distance)
+    return dijkstraLens
+
+if __name__ == "__main__":
+    graph = createGraph(fileName)
+    seenVertex = {}
+    graph[1]['length'] = 0
+    heapGraph = [(graph[vertex]['length'], (vertex, graph[vertex]['children'])) for vertex in graph.keys()]
+    #pp.pprint(heapGraph)
+    heapq.heapify(heapGraph)
+    #pp.pprint(heapGraph)
+    #print(heapq.heappop(heapGraph))
+    #sys.exit()
+    while heapGraph:
+        minLength, minElement = heapq.heappop(heapGraph)
+        minVertex, minVertexChildren = minElement
+        seenVertex.add(minVertex)
+        graph[minVertex]['length'] = minLength
+
+        for child, length in minVertexChildren.items():
+            heapGraph[]
+
+
+
+    #pp.pprint(graph)
+    nodeMap = map(int, "7,37,59,82,99,115,133,165,188,197".split(","))
+    nodeList = list(nodeMap)
+    lengths = [str(graph[node]["length"]) for node in nodeList]
+    #lengths = [str(graph[node]["length"]) for node in nodeList]
+    lenString = ",".join(lengths)
+    print("networkx result")
+    pp.pprint(dijkstraPathNetworkx(graph, nodeList))
+    print("my result")
+    pp.pprint(lenString)
+
+
