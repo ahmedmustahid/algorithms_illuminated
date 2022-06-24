3c29cb307f3ec24fcd500767fddcd7ecfc5b975d
ahmedmustahid <einsteinium25@gmail.com>
2022-06-24 15:14:42 +0900

    implemented extractMin

--- graph_algorithm/Dijkstra/heap.py
+++ graph_algorithm/Dijkstra/heap.py
@@ -1,6 +1,7 @@
 import math
 import pprint
 import heapq
+import time
 pp = pprint.PrettyPrinter(width=41, compact=True)
 def insert(heap, key):
     if heap:
@@ -16,20 +17,56 @@ def insert(heap, key):
 
     return heap
 
+def extractMin(heap):
+    minimumElement = heap[0]
+    heap[0] = heap.pop()
+    parentIndex = 0
+    firstChildIndex, secondChildIndex = 2 * parentIndex + 1, 2 * parentIndex + 2
+
+    while heap[parentIndex] > heap[firstChildIndex] or heap[parentIndex] > heap[secondChildIndex]:
+        if heap[firstChildIndex] < heap[secondChildIndex]:
+            heap[firstChildIndex], heap[parentIndex] = heap[parentIndex], heap[firstChildIndex]
+            parentIndex = firstChildIndex
+            firstChildIndex, secondChildIndex = 2 * parentIndex + 1, 2 * parentIndex + 2
+        elif heap[secondChildIndex] <= heap[firstChildIndex]:
+            heap[secondChildIndex], heap[parentIndex] = heap[parentIndex], heap[secondChildIndex]
+            parentIndex = secondChildIndex
+            firstChildIndex, secondChildIndex = 2 * parentIndex + 1, 2 * parentIndex + 2
+
+        if secondChildIndex >= len(heap) -1:
+            break
+    return minimumElement, heap
+
 if __name__=="__main__":
     keys = list(range(9,0,-1))
     print("original keys")
     pp.pprint(keys)
+    start = time.time()
     heap = []
     for key in keys:
         heap = insert(heap, key)
         #pp.pprint(heap)
+    print(f"time for my heap insert implementation {time.time() - start}")
     print("my heap insertion")
     pp.pprint(heap)
     print("python heapify")
+    start = time.time()
     heapq.heapify(keys)
+    print(f"time for python heapify implementation {time.time() - start}")
     pp.pprint(keys)
 
     print("applying heapify to my heap insertion")
     heapq.heapify(heap)
     pp.pprint(heap)
+
+    heap2 = heap[:]
+
+    minimumElement, newHeap = extractMin(heap)
+    print(f"minimumElement my implementation {minimumElement}")
+    print("new heap my implementation")
+    pp.pprint(newHeap)
+
+    minimumElementHeapq = heapq.heappop(heap2)
+    print(f"minimumElement heapq {minimumElementHeapq}")
+    print("new heap heapq implementation")
+    pp.pprint(heap2)
