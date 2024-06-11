c6a8b1920812d273e1e6325372d940553af4cc3b
ahmedmustahid <einsteinium25@gmail.com>
2024-06-11 15:57:21 +0900

    passed challenge case for meadian maintenance

--- heaps_binary_trees/medianMaintenance.py
+++ heaps_binary_trees/medianMaintenance.py
@@ -39,78 +39,55 @@ class MedianHeap:
 
     @staticmethod
     def rebalance(h1: MaxHeap, h2: MinHeap) -> Tuple[MaxHeap, MinHeap]:
-        # print(":::::::::::::::::::::::::::")
-        # print(f"maxheap {h1}")
-        # print(f"minheap {h2}")
-        # print("666666666666666666")
         if len(h1) > len(h2):
             temp = h1.extract()
             h2.insert(temp)
         else:
             temp = h2.extract()
             h1.insert(temp)
-        # print(f"maxheap {h1}")
-        # print(f"minheap {h2}")
-        # print(":::::::::::::::::::::::::::")
+
         return h1, h2
 
 
-if __name__ == "__main__":
+def testCase():
     mh = MedianHeap()
     tmplist: List[int] = []
 
     elems = [random.randint(1, 10000) for _ in range(1000)]
-    # elems = [
-    #     578,
-    #     193,
-    #     354,
-    #     66,
-    #     678,
-    #     870,
-    #     848,
-    #     649,
-    #     151,
-    #     225,
-    #     113,
-    #     267,
-    #     257,
-    #     934,
-    #     614,
-    #     977,
-    #     967,
-    #     291,
-    #     190,
-    #     306,
-    #     539,
-    #     522,
-    #     134,
-    #     145,
-    #     190,
-    #     58,
-    # ]
+
     for i, elem in enumerate(elems):
-        # print(f"{i}th elem to insert {elem}")
         mh.insert(elem)
         tmplist.append(elem)
 
-        # print(f"tmpList {tmplist}")
-        # print(f"h1: {mh.h1}")
-        # print(f"h2: {mh.h2}")
         mymedian = mh.getMedian()
-        print(f"median {mymedian}")
+        print(f"{i}th median {mymedian}")
         pymedian = statistics.median_low(tmplist)
         print(f"python median low:{pymedian}")
         assert mymedian == pymedian
         print("---------------------")
 
-    # elems = [1, 84, 13, 55, 11, 22, 7, 16, 27, 62, 46, 28, 27, 42, 95, 56]
-    # for elem in elems:
-    #     mh.insert(elem)
-    #     tmplist.append(elem)
 
-    #     print(f"tmpList {tmplist}")
-    #     print(f"h1: {mh.h1}")
-    #     print(f"h2: {mh.h2}")
-    #     print(f"median {mh.getMedian()}")
-    #     print(f"python median low:{statistics.median_low(tmplist)}")
-    #     print("---------------------")
+def challengeCase() -> int:
+    mh = MedianHeap()
+    tmplist: List[int] = []
+    medians: List[int] = []
+    with open("median.txt", "r") as f:
+        for i, line in enumerate(f):
+            elem = int(line)
+            tmplist.append(elem)
+            mh.insert(elem)
+            median = mh.getMedian()
+            medians.append(median)
+            # pymedian = statistics.median_low(tmplist)
+            # assert pymedian == median
+            # print(f"python median low:{pymedian}")
+            print(f"{i}th median {median}")
+    s = sum(medians)
+    mod = s % 10000
+
+    print(f"mod: {mod}")
+    return mod
+
+
+if __name__ == "__main__":
+    challengeCase()
