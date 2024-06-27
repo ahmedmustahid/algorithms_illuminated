ab8462684296cf30d85442b782ecc3265b52f939
ahmedmustahid <einsteinium25@gmail.com>
2024-06-27 12:17:43 +0900

    passed the challenge case

--- greedy_algorithms/prim_algorithm.py
+++ greedy_algorithms/prim_algorithm.py
@@ -6,6 +6,9 @@ from typing import List, Dict, Tuple, Set
 if __name__ == "__main__":
     testDir = "test_cases/prob3"
     fname = f"{testDir}/input_random_10_40.txt"
+    fname = f"{testDir}/input_random_33_1000.txt"
+    fname = f"{testDir}/challenge_case.txt"
+    # fname = f"{testDir}/pg59.txt"
 
     graphDict: Dict[str, List[Tuple[str, int]]] = defaultdict(list)
     graphDict = getGraph(fname)
@@ -23,42 +26,27 @@ if __name__ == "__main__":
 
     while not q.empty():
         key = q.get()
-        for value in graphDict[key]:
-            node = value[0]
-            cost = value[1]
+        for node, cost in graphDict[key]:
             if node not in X:
-                temp_key1 = (key, node)
-                temp_key2 = (node, key)
-                if temp_key1 not in candidates or temp_key2 not in candidates:
-                    candidates[(key, value[0])] = value[1]
+                candidates[(key, node)] = cost
+
+        candidatesCopy = candidates.copy()
+        for k, _ in candidatesCopy.items():
+            n1, n2 = k
+            if n1 in X and n2 in X:
+                candidates.pop(k)
+
+        if candidates:
+            minedge = min(candidates.items(), key=lambda x: x[1])
+            minNode = minedge[0][1]
+            X.add(minNode)
+            q.put(minNode)
+            T.append(minedge[0])
+            minCostSum += candidates[minedge[0]]
+            candidates.pop(minedge[0])
+
         print(candidates)
-        minNode: Tuple[str, str] = min(candidates, key=candidates.get)  # type: ignore
-        print(minNode)  # type: ignore
-        X.add(minNode[1])  # type: ignore
-        minCostSum += candidates[minNode]
-        print(minCostSum)
-        candidates.pop(minNode)  # type: ignore
-
-        T.append(minNode)  # type: ignore
+        print(X)
         print(T)
-
-        q.put(minNode[1])  # type: ignore
-
-    # for key, values in graphDict.items():
-    #     if key in X:
-    #         for value in values:
-    #             if value[0] not in X:
-    #                 temp_key1 = (key, value[0])
-    #                 temp_key2 = (value[0], key)
-    #                 if temp_key1 not in candidates or temp_key2 not in candidates:
-    #                     candidates[(key, value[0])] = value[1]
-    #         print(candidates)
-    #         minNode: Tuple[str, str] = min(candidates, key=candidates.get)  # type: ignore
-    #         print(minNode)  # type: ignore
-    #         X.add(minNode[1])  # type: ignore
-    #         minCostSum += candidates[minNode]
-    #         print(minCostSum)
-    #         candidates.pop(minNode)  # type: ignore
-
-    #         T.append(minNode)  # type: ignore
-    #         print(T)
+        print(minCostSum)
+        print("---------------------------------------")
