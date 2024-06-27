fc7b0300dd4a6e501086af745730b2cfcd9224e2
ahmedmustahid <einsteinium25@gmail.com>
2024-06-27 12:17:13 +0900

    use networkx to verify the graph construction

--- greedy_algorithms/create_graph.py
+++ greedy_algorithms/create_graph.py
@@ -1,27 +1,59 @@
 from collections import defaultdict
 from typing import List, Dict, Tuple
 
+import networkx as nx
+
 
 def getGraph(fname: str):
-    graph: List[Tuple[str, Tuple[str, int]]] = []
+    graph: List[Tuple[str, str, int]] = []
     graphDict: Dict[str, List[Tuple[str, int]]] = defaultdict(list)
     with open(fname, "r") as f:
-        for i, line in enumerate(f):
-            if i == 0:
+        for line in f:
+            if len(line.split()) < 3:
                 continue
             v1, v2, weight = line.split()
             weight = int(weight)
-            temp = (v1, (v2, weight))
+            temp = (v1, v2, weight)
             graph.append(temp)
 
-    for key, value in graph:
-        graphDict[key].append(value)
+    for v1, v2, weight in graph:
+        graphDict[v1].append((v2, weight))
+        graphDict[v2].append((v1, weight))
 
     return graphDict
 
 
+def getNxGraph(fname: str):
+    graph: List[Tuple[str, str, Dict[str, int]]] = []
+    with open(fname, "r") as f:
+        for line in f:
+            if len(line.split()) < 3:
+                continue
+            v1, v2, weight = line.split()
+            weight = int(weight)
+            temp = (v1, v2, {"weight": weight})
+            graph.append(temp)
+
+    G = nx.Graph()
+    for g in graph:
+        e1, e2, wt = g
+        G.add_edge(e1, e2, weight=wt["weight"])
+
+    return G
+
+
 if __name__ == "__main__":
     testDir = "test_cases/prob3"
-    fname = f"{testDir}/edges_prob3.txt"
+    fname = f"{testDir}/pg59.txt"
+    fname = f"{testDir}/input_random_33_1000.txt"
+    fname = f"{testDir}/challenge_case.txt"
 
-    print(getGraph(fname))
+    # print(getGraph(fname))
+    G = getNxGraph(fname)
+    G = nx.minimum_spanning_tree(G)
+    s = sorted(G.edges(data=True))
+    mincost = sum(
+        [e[2]["weight"] for e in s]
+    )  # [('1', '2', {'weight': 1}), ('1', '3', {'weight': 4}), ('2', '4', {'weight': 2})]
+    print(s)
+    print(f"mincost {mincost}")
