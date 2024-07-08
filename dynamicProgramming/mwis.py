a3c903edf648a99e68d8a126fef865516693ddfe
ahmedmustahid <einsteinium25@gmail.com>
2024-07-08 14:30:44 +0900

    passed test case of mwis

---
+++ dynamicProgramming/mwis.py
@@ -0,0 +1,68 @@
+from typing import List, Set
+
+
+def getWeights(fname: str) -> List[int]:
+    weights = [0]
+
+    with open(fname, "r") as f:
+        for i, line in enumerate(f):
+            if i == 0:
+                continue
+            weights.append(int(line))
+    return weights
+
+
+def getMaxIWeights(weights: List[int]):
+    n = len(weights)
+    A = list((0 for _ in range(n)))
+    A[0], A[1] = 0, weights[1]
+    for i in range(2, n):
+        A[i] = max(A[i - 1], A[i - 2] + weights[i])
+    return A
+
+
+def getMWISNodes(maxWeights: List[int], weights: List[int]) -> Set[int]:
+    S: Set[int] = set()
+    i = len(maxWeights) - 1
+
+    while i >= 2:
+        if maxWeights[i - 1] >= maxWeights[i - 2] + weights[i]:
+            i = i - 1
+        else:
+            S.add(i)
+            i = i - 2
+    if i == 1:
+        S.add(i)
+
+    return S
+
+
+def mwis(fname: str):
+    weights = getWeights(fname)
+    A = getMaxIWeights(weights)
+    S = getMWISNodes(A, weights)
+    return S
+
+
+def bitsFromStr(S: Set[int], st: str):
+    arr = st.split(",")
+    arr = list(map(int, arr))
+
+    temp = ""
+    for e in arr:
+        if e in S:
+            temp += "1"
+        else:
+            temp += "0"
+    return temp
+
+
+if __name__ == "__main__":
+    root = "test_cases"
+    st = "1,2,3,4,17,117,517,997"
+    fname = "mwis.txt"
+    fname = "input_random_29_1000.txt"
+    fname = f"{root}/{fname}"
+
+    S = mwis(fname)
+    print(bitsFromStr(S, st))
