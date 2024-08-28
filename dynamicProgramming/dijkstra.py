9425886fa4d2c604061ad68cd366fa2c94d1b89b
ahmedmustahid <einsteinium25@gmail.com>
2024-08-28 16:05:39 +0900

    correct implementation of heap based dijkstra

--- dynamicProgramming/dijkstra.py
+++ dynamicProgramming/dijkstra.py
@@ -1,6 +1,21 @@
 from typing import Dict
 import heapq
+from pathlib import Path
+import networkx as nx
 from collections import defaultdict
+
+
+def createGraph1(fileName):
+    graph = defaultdict(lambda: {"children": {}, "length": float("inf")})
+    with open(fileName) as f:
+        for line in f:
+            test = line.split()
+            for child in test[1:]:
+                node, weight = map(int, child.split(","))
+                graph[test[0]]["children"][str(node)] = weight
+    # pp.pprint(graph)
+    # sys.exit()
+    return graph
 
 
 def getGraph(fname: str) -> defaultdict[str, Dict[str, int]]:
@@ -25,7 +40,7 @@ def createGraph(fileName):
             head, tail, cost = line.split()
             graph[head]["children"][tail] = int(cost)
             children.add(tail)
-    print()
+    # print()
     noChildNodes = children - set(graph.keys())
     if noChildNodes:
         for nc in noChildNodes:
@@ -63,31 +78,89 @@ def getIdx(H, child):
     return None
 
 
-if __name__ == "__main__":
-    fname = "test_cases/dijkstraSmall.txt"
-    graph = createGraph(fname)
+def createNetworkx(graph, tails):
+    G = nx.DiGraph()
+    # graph = createGraph(fileName)
+
+    for head in graph:
+        for tail, weight in graph[head]["children"].items():
+            G.add_edge(head, tail, weight=weight)
+    dijkstraLens = []
+    for tail in tails:
+        distance, _ = nx.single_source_dijkstra(G, source="1", target=tail)
+        dijkstraLens.append(distance)
+    return dijkstraLens
+
+
+def reconstruction(prev, target):
+    path = [target]
+    while target != "1":
+        target = prev[target]
+        path.append(target)
+    path.reverse()
+    return path
+
+
+def dijkstra(fileName, target=None):
+    graph = createGraph1(fileName)
+
     graph["1"]["length"] = 0
 
     X = set()
-    l = []
+    path = []
+    mins = {"1": 0}
+
+    prev = {}
     H = []
     for node in graph.keys():
-        key = graph[node]["length"]
-        H.append((key, node))
+        cost = graph[node]["length"]
+        H.append((cost, node))
     heapq.heapify(H)
-    print(H)
+
     while H:
         cost, node = heapq.heappop(H)
         X.add(node)
-        l.append(node)
+        mins[node] = cost
+        if target and node == target:
+            path = reconstruction(prev, target)
+            return path, mins
 
         for c in graph[node]["children"].keys():
+            if c in X:
+                continue
             k, idx = getIdx(H, c)
             H = heapqDelete(H, idx)
-            print(H)
             lnc = cost + graph[node]["children"][c]
             k = min(k, lnc)
+            prev[c] = node
             heapq.heappush(H, (k, c))
 
-            print(H)
-    print(l)
+    return path, mins
+
+
+if __name__ == "__main__":
+    # fileName = Path.home()/"work/algorithms_illuminated/graph_algorithm/Dijkstra/testCases/dijkstraChallenge.txt"
+    fileName = (
+        Path.home()
+        / "work/algorithms_illuminated/graph_algorithm/Dijkstra/testCases/testCase1.txt"
+    )
+    fileName = (
+        Path.home()
+        / "work/algorithms_illuminated/graph_algorithm/Dijkstra/testCases/dijkstraChallenge.txt"
+    )
+
+    nodelist = "1,2,3,4,5,6,7,8".split(",")
+    nodelist = "7,37,59,82,99,115,133,165,188,197".split(",")
+
+    costs = []
+    for node in nodelist:
+        _, c = dijkstra(fileName=fileName, target=node)
+        # costs.append(c)
+        # print(f"node {node}, path:{p}, cost:{c}")
+    for node in nodelist:
+        costs.append(str(c[node]))
+    print(",".join(costs))
+    # p, c = dijkstra(fileName=fileName)
+    # print(f"node {node}, path:{p}, cost:{c}")
+    graph = createGraph1(fileName)
+    print(createNetworkx(graph, tails=nodelist))
