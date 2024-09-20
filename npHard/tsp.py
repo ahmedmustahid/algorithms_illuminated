0aab191eb57f8c66ddbccbbc04ae44e5ea47a828
ahmedmustahid <einsteinium25@gmail.com>
2024-09-20 12:08:12 +0900

    replaced print with logging debug

--- npHard/tsp.py
+++ npHard/tsp.py
@@ -2,8 +2,11 @@ from utils import getXYs, getDistances, globalL, getSubsets, createBinarySeq, Mi
 from pathlib import Path
 from typing import Tuple, Dict, List, DefaultDict, Set
 import heapq
-import pprint
-import random
+from pprint import pformat
+import logging
+
+
+logging.basicConfig(filename='myapp.log',filemode="w", level=logging.DEBUG)
 DIST_THRESH=0
 def deleteSmalldists(xyDict, idToxy):
     delKeys = {xy for xy in xyDict.keys() if xyDict[xy]<DIST_THRESH}
@@ -26,10 +29,11 @@ def bellmanHeldKarp(xyDistDict:Dict[Tuple[int, int], float], subsets: DefaultDic
     for k,v in xyDistDict.items():
         if 1 in k:
             A[k] = v
-    pprint.pprint(f"A {A}")
+    logging.debug(f"A {pformat(A)}")
+    
 
     test = {frozenset(k):v for k,v in xyDistDict.items() if 1 in k}
-    print(minNodes)
+    # print(minNodes)
     maxSize = max(subsets)
 
     for size in range(3, maxSize+1):
@@ -40,59 +44,60 @@ def bellmanHeldKarp(xyDistDict:Dict[Tuple[int, int], float], subsets: DefaultDic
             for j in subset - {1}:
                 oldKey = subset - {j}
                 for k in subset - {1} - {j}:
-                    print("------------")
-                    print(f"k:{k},j:{j}")
-                    print(f"subset {subset}")
-                    print(f"oldKey: {oldKey}[{k}]")
-                    print(f"newKey: {subset}[{j}]")
+                    logging.debug("------------")
+                    logging.debug(f"k:{k},j:{j}")
+                    logging.debug(f"subset {subset}")
+                    logging.debug(f"oldKey: {oldKey}[{k}]")
+                    logging.debug(f"newKey: {subset}[{j}]")
                     startNode = minNodes[frozenset(oldKey)][1]
                     if k!=startNode:
                         continue
                     endNode = j
-                    print(f"startnode:{startNode}, endNode: {endNode}")
+                    logging.debug(f"startnode:{startNode}, endNode: {endNode}")
                     if size < maxSize:
                         newval = test[frozenset(oldKey)] + xyDistDict[frozenset((startNode, endNode))]
-                        print(f"newval: {test[frozenset(oldKey)]}+{xyDistDict[frozenset((startNode, endNode))]}")
+                        logging.debug(f"newval: {test[frozenset(oldKey)]}+{xyDistDict[frozenset((startNode, endNode))]}")
                     else:
                         newval = test[frozenset(oldKey)] + xyDistDict[frozenset((startNode, endNode))] + xyDistDict[frozenset((endNode, 1))]
-                        print(f"newval: {test[frozenset(oldKey)]}+{xyDistDict[frozenset((startNode, endNode))]};{xyDistDict[frozenset((endNode, 1))]}")
+                        logging.debug(f"newval: {test[frozenset(oldKey)]}+{xyDistDict[frozenset((startNode, endNode))]};{xyDistDict[frozenset((endNode, 1))]}")
 
                     newval = round(newval,2)
                     heapq.heappush(h, (newval, (k,j)))
-                    print("------------")
+                    # print("------------")
             minVal, minNode = heapq.heappop(h)
             minNodes[frozenset(subset)] = minNode
             test[frozenset(subset)] = minVal
 
-            print(f"minVal {minVal}, minNode {minNode}")
-            pprint.pprint(minNodes)
-            print("======")
-            pprint.pprint(test)
-            print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
+            logging.debug(f"minVal {minVal}, minNode {minNode}")
+            logging.debug(f"minNodes\n{pformat(minNodes)}")
+            # print("======")
+            logging.debug(f"test\n{pformat(test)}")
+            # print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
 
 
 if __name__=="__main__":
     root = Path.home() / "work/algorithms_illuminated/npHard/test_cases"
     fname = "tsp.txt"
-    fname = "input_float_10_4.txt"
     fname = "input_float_11_4.txt"
     fname = "input_float_12_4.txt"
     fname = "input_float_20_6.txt"
+    fname = "input_float_10_4.txt"
     fpath = root / fname
 
     xs, ys, idToxy = getXYs(str(fpath))
     xyDistDict, dists = getDistances(idToxy)
     # pprint.pprint(f"dists {dists}")
-    pprint.pprint(xyDistDict)
+    # pprint.pprint(xyDistDict)
 
     idToxy = deleteSmalldists(xyDistDict, idToxy)
     m=len(idToxy)
     l = [0 for _ in range(m)]
     createBinarySeq(m, l)
-    print(f"binary digits lens: {len(globalL)}")
+    # print(f"binary digits lens: {len(globalL)}")
     subsets = getSubsets(list(idToxy.keys()), globalL)
-    pprint.pprint(subsets)
-    print(f"xys len: {len(list(idToxy.keys()))}")
-    print(f"random subset : {subsets[2][:10]}")
+    # pprint.pprint(subsets)
+    # print(f"xys len: {len(list(idToxy.keys()))}")
+    # print(f"random subset : {subsets[2][:10]}")
 
     bellmanHeldKarp(xyDistDict, subsets)
