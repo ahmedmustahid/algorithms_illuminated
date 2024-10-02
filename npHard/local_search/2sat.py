794c94780babeb6ed149c7ad6afc349f57b861a8
ahmedmustahid <einsteinium25@gmail.com>
2024-10-03 06:33:38 +0900

    correctly terminates after 2sat

--- npHard/local_search/2sat.py
+++ npHard/local_search/2sat.py
@@ -7,26 +7,12 @@ from pathlib import Path
 import pprint
 import sys
 
-def randomLiteralBit(literalBitDict, clausesDict):
-    for key in literalBitDict:
-        if key>0:
-            literalBitDict[key] = random.choice((0,1))
-            if -key in literalBitDict:
-                literalBitDict[-key] = 0 if literalBitDict[key]==1 else 1
-        elif -key not in literalBitDict:
-                literalBitDict[key] = random.choice((0,1))
-
-
-    
+def randomClausesBit(clausesDict):
     for key in clausesDict.keys():
-        if len(key)==1:
-            x = next(iter(key))
-            clausesDict[key] = literalBitDict[x]
-        else:
-            x1, x2 = key
-            clausesDict[key] = literalBitDict[x1] or literalBitDict[x2]
-    return literalBitDict, clausesDict
-
+        clausesDict[key] = random.choice((0,1))
+    
+    return clausesDict
+    
 def getZeroClause(clausesDict):
     zeroClauses = []
     for k,v in clausesDict.items():
@@ -40,38 +26,45 @@ def flipBit(literal, literalBitDict):
         literalBitDict[-literal] = 0 if literalBitDict[literal]==1 else 1
     return literalBitDict
 
+def findClausesFromList(literal,literalsNumToIdx):
+    clausePositions = []
+    if literal in literalsNumToIdx:
+        clausePositions = literalsNumToIdx[literal]
+    if -literal in literalsNumToIdx:
+        clausePositions+= literalsNumToIdx[-literal]
+    return clausePositions
+
+def evaluateClausesFromNewBitDict(clausePositions, literalsList, literalBitDict):
+    clausePositions = set(clausePositions)
+    for clausePosition in clausePositions:
+        clause = literalsList[clausePosition]
+        if len(clause)==1:
+            x = next(iter(clause))
+            clausesDict[(x,)] = literalBitDict[x]
+        else:
+            x1, x2 = clause
+            clausesDict[clause] = literalBitDict[x1] or literalBitDict[x2]
+    return clausesDict
+
 
 def papadimitrou(literalsList, literalsNumToIdx, literalBitDict, clausesDict):
     n = len(literalsList)
     logn = math.ceil(math.log2(n))
 
     for i in range(logn):
-        literalBitDict, clausesDict = randomLiteralBit(literalBitDict, clausesDict)
+        clausesDict = randomClausesBit(clausesDict)
         for j in range(2*n**2):
             zeroClauses = getZeroClause(clausesDict)
             
             if zeroClauses:
                 zeroClause = zeroClauses.pop()
-                print(f"zeroclause {zeroClause}")
+                # print(f"zeroclause {zeroClause}")
                 literal = random.choice(zeroClause)
 
                 literalBitDict = flipBit(literal, literalBitDict)
                 
-                clausePositions = []
-                if literal in literalsNumToIdx:
-                    clausePositions = literalsNumToIdx[literal]
-                if -literal in literalsNumToIdx:
-                    clausePositions+= literalsNumToIdx[-literal]
-
-                clausePositions = set(clausePositions)
-                for clausePosition in clausePositions:
-                    clause = literalsList[clausePosition]
-                    if len(clause)==1:
-                        x = next(iter(clause))
-                        clausesDict[(x,)] = literalBitDict[x]
-                    else:
-                        x1, x2 = clause
-                        clausesDict[clause] = literalBitDict[x1] or literalBitDict[x2]
+                clausePositions = findClausesFromList(literal, literalsNumToIdx)
+                clausesDict = evaluateClausesFromNewBitDict(clausePositions, literalsList, literalBitDict)
             else:
                 return 1, literalBitDict, clausesDict
 
@@ -79,17 +72,16 @@ def papadimitrou(literalsList, literalsNumToIdx, literalBitDict, clausesDict):
 
 
 
-
-
-
-
-
 if __name__=="__main__":
     p = Path.home()/"work/algorithms_illuminated/npHard/test_cases/local_search"
-    fname = "2sat1.txt"
+    fname = "small.txt"
+    fname = "input_beaunus_1_2.txt"
     fname = "input_beaunus_10_20.txt"
     fname = "input_beaunus_11_40.txt"
-    fname = "input_beaunus_1_2.txt"
+    fname = "2sat1.txt"
+    fname = "2sat2.txt"
+    fname = "input_beaunus_13_80.txt"
+    fname = "input_beaunus_23_1000.txt"
     fname = p/fname
 
 
